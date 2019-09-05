4bb6ed25dbe962046b2cf07f9378f487975dcd52
Rubens Leme <rubenss.leme@gmail.com>
2019-09-04 22:38:23 -0300

    projeto android inicio

---
+++ app/src/main/java/br/com/vitorprimeiroapp/projetomodelo/ActivitySobre.java
@@ -0,0 +1,14 @@
+package br.com.vitorprimeiroapp.projetomodelo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivitySobre extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sobre);
+    }
+}
